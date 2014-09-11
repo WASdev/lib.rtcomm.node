fe7cc9adf00495bfd231881dfe2b795ac93b68ca
Brian Pulito <brian_pulito@us.ibm.com>
2014-09-11 17:05:43 -0400

    Fixed a few issues related to the test for this module and the latest transition to RtcConnector.

--- tests/RtcConnector.js
+++ tests/RtcConnector.js
@@ -17,8 +17,8 @@ define([
     'intern!object',
     'intern/chai!assert',
     'intern/dojo/node!util',
-    'intern/dojo/node!../lib/EventMonitor'
-], function (registerSuite, assert, util, eventMonitor) {
+    'intern/dojo/node!../lib/RtcConnector'
+], function (registerSuite, assert, util, rtcConnector) {
 
     var eventPath = '/rtcomm/event';
 
@@ -40,10 +40,10 @@ define([
         ];
 
     registerSuite({
-        name: 'EventMonitor',
+        name: 'RtcConnector',
         // before the suite starts
         setup: function() {
-          em = eventMonitor.get(config1);
+          em = rtcConnector.get(config1);
         },
         // before each test executes
         beforeEach: function () {
@@ -51,47 +51,47 @@ define([
         },
         // after the suite is done
         teardown: function() {
-          eventMonitor.close();
+          rtcConnector.close();
         },
         'Create second monitor with same config are equal': function () {
-            var em2 = eventMonitor.get(config1);
+            var em2 = rtcConnector.get(config1);
             assert.strictEqual(em, em2,
-                'EventMonitor  should return same object if same config ');
-            assert.strictEqual(eventMonitor.list().length, 1,
+                'RtcConnector  should return same object if same config ');
+            assert.strictEqual(rtcConnector.list().length, 1,
               'only 1 Monitor listed');
             
         },
         'Create a second monitor with different config': function() {
-            var em2 = eventMonitor.get(config2);
+            var em2 = rtcConnector.get(config2);
             assert.notStrictEqual(em, em2,
-                'EventMonitor should return a different object if a different configuration ');
-            assert.strictEqual(eventMonitor.list().length, 2);
-//            console.log(eventMonitor.list());
+                'RtcConnector should return a different object if a different configuration ');
+            assert.strictEqual(rtcConnector.list().length, 2);
+//            console.log(rtcConnector.list());
         },
         'delete a monitor': function() {
           var monitortodelete = null;
-          var size = eventMonitor.list().length;
+          var size = rtcConnector.list().length;
           assert.strictEqual(size, 2);
           console.log('****em****');
           console.log(em);
-          eventMonitor.list().forEach(function(id) {
+          rtcConnector.list().forEach(function(id) {
             console.log('****'+id+'****');
-            if (eventMonitor[id] !== em.id)  {
-              monitortodelete = eventMonitor.find(id);
+            if (rtcConnector[id] !== em.id)  {
+              monitortodelete = rtcConnector.find(id);
             }
           });
 
-          eventMonitor.delete(monitortodelete);
-          assert.strictEqual(eventMonitor.list().length, 1, 'Monitor was successfully deleted');
+          rtcConnector.delete(monitortodelete);
+          assert.strictEqual(rtcConnector.list().length, 1, 'Monitor was successfully deleted');
         },
         'Force Unique Client': function() {
           var c1 = Object.create(config1);
           c1.unique = true;
-          var size = eventMonitor.list().length;
+          var size = rtcConnector.list().length;
           assert.strictEqual(size, 1);
-          var emUnique1 = eventMonitor.get(c1);
+          var emUnique1 = rtcConnector.get(c1);
           assert.strictEqual(emUnique1.key, em.key, 'Create two unique Event Monitors with same configuration');
-          eventMonitor.delete(emUnique1);
+          rtcConnector.delete(emUnique1);
 
         },
 
