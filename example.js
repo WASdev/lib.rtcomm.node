33eb4c3acd98f40dda7fb01c4eda16279b8497f3
Scott W Graham <swgraham@us.ibm.com>
2015-12-02 09:42:14 -0700

    update to example.js

--- example.js
+++ example.js
@@ -1,2 +1,11 @@
 var rtcomm = require('./lib/rtcomm');
 
+var config = {
+  server: "messagesight.demos.ibm.com",
+  port : 1883,
+}
+
+var endpointConnection = new rtcomm.EndpointConnection(config);
+
+endpointConnection.connect();
+console.log(endpointConnection);
